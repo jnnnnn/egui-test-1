be0545d71b79d4ef83055b9cd544d87e2c58ec8c
jnnnnn <jnnnnn@gmail.com>
2023-09-30 11:44:49 +1300

    much better

--- sql/compress-fiction.sql
+++ sql/compress-fiction.sql
@@ -1,24 +1,18 @@
--- 3 GB
-
--- 1.6m English, 0.9m everything else
-
--- from this point on, we become incompatible with the standard layout
--- this requires the compresseddb flag in the config so that the query doesn't try to join to fiction_hashes
+PRAGMA synchronous = OFF;
+PRAGMA journal_mode = MEMORY;
 
 CREATE TABLE fiction_mini AS 
 select fh.ipfs_cid, f.Title, f.Author, f.Series, f.Language, f.Year, f.Publisher, f.Extension, f.Filesize, f.Locator
 from fiction f
 join fiction_hashes fh on fh.md5 = lower(f.md5)
-where fh.ipfs_cid is not null and trim(fh.ipfs_cid) <> ""
-
+where fh.ipfs_cid is not null and trim(fh.ipfs_cid) <> ''
+;
 CREATE INDEX "idx_fiction_mini_Search" ON "fiction_mini" (`Author`, `Title`, `Series`, `Language`, `Extension`)
+;
+drop table fiction ;
+drop table fiction_description ;
+drop table fiction_hashes ;
 
-drop table fiction
-
-drop table fiction_description
-
-drop table fiction_hashes
-
-VACUUM 
+VACUUM;
 
 -- 0.5 GB
